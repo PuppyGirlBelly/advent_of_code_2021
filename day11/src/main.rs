52e25411029e9b45ddf864a57b415ec3107643d4
corless <jazzchamp600@gmail.com>
2021-12-11 01:02:15 -0700

    Initalize day 11

---
+++ day11/src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{BufRead, BufReader, Lines};
+use std::fs::File;
+// use std::fs::read_to_string;
+use std::time::Instant;
+
+fn main() {
+    let start: Instant = Instant::now();
+    let file: File = File::open(String::from("src/input.txt")).unwrap();
+    let reader: BufReader<File> = BufReader::new(file);
+    let lines: Lines<BufReader<File>> = reader.lines();
+    // let mut lines: Peekable<Lines<BufReader<File>>> = reader.lines().peekable();
+    // let input: Vec<_> = file_to_vec("src/input.txt");
+
+    for _line in lines {
+    }
+
+    println!("This main took: {:?}", Instant::now().duration_since(start));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; 
+
+    #[test]
+    fn test() {
+        assert!(true);
+    }
+
+}
